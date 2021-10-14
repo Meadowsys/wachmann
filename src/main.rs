86810a9712c3564f7aafd4ff32ad2f9d203e0d70
Autumn <blazeykirin@gmail.com>
2021-10-14 02:10:26 +0000

    install tokio runtime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-	println!("Hello, world!");
+// todo remove this when wachmann is more finished
+#![allow(unused)]
+
+use std::error::Error;
+use std::time::Duration;
+
+type MainResult = Result<(), Box<dyn Error + Send + Sync>>;
+
+fn main() -> MainResult {
+	let rt = tokio::runtime::Builder::new_multi_thread()
+		.enable_all()
+		.worker_threads(2)
+		.max_blocking_threads(32)
+		.thread_keep_alive(Duration::from_secs(60))
+		.build()
+		.unwrap();
+
+	rt.block_on(async_main())?;
+	rt.shutdown_timeout(Duration::from_secs(60));
+
+	Ok(())
+}
+
+async fn async_main() -> MainResult {
+	println!("runtime works lol");
+
+	Ok(())
 }
