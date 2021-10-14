f7a9bf672fa7c6905f2a93bb2922922e4e21b2ca
Autumn <blazeykirin@gmail.com>
2021-10-14 04:19:51 +0000

    it connects and does nothing now, yeet

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,23 @@ mod env;
 use std::error::Error;
 use std::time::Duration;
 
+use twilight_model::channel::message::{
+	AllowedMentions,
+	allowed_mentions::AllowedMentionsBuilder
+};
+
+use twilight_http::{
+	Client as HttpClient,
+	client::ClientBuilder as HttpClientBuilder
+};
+
+use twilight_gateway::{
+	cluster::{ Cluster, ShardScheme::Auto },
+	Event, Intents
+};
+
+use futures::stream::StreamExt;
+
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 	let rt = tokio::runtime::Builder::new_multi_thread()
 		.enable_all()
@@ -18,13 +35,57 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 	rt.block_on(async_main())?;
 	rt.shutdown_timeout(Duration::from_secs(60));
 
+	println!("down!");
+
 	Ok(())
 }
 
 async fn async_main() -> Result<(), Box<dyn Error + Send + Sync>> {
-	println!("runtime works lol");
+	println!("starting up...");
 
 	let env = env::Env::get_env()?;
+
+	let allowed_mentions = AllowedMentionsBuilder::new()
+		.users()
+		.build();
+
+	let http = HttpClientBuilder::new()
+		.default_allowed_mentions(allowed_mentions)
+		.build();
+
+	let intents = Intents::all();
+
+	let (cluster, mut events) = Cluster::builder(env.token(), intents)
+		.shard_scheme(Auto)
+		.build().await?;
+
+	cluster.up().await;
+	println!("up!");
+
+	let cluster_down = cluster.clone();
+	tokio::spawn(async move {
+		use tokio::signal::unix::{ signal, SignalKind as SK };
+		let mut sigint = signal(SK::interrupt()).unwrap();
+		let mut sigterm = signal(SK::terminate()).unwrap();
+
+		tokio::select! {
+			biased;
+
+			_ = sigint.recv() => {
+				println!("received SIGINT, shutting down...");
+			}
+			_ = sigterm.recv() => {
+				println!("received SIGTERM, shutting down...");
+			}
+		}
+
+		cluster_down.down();
+	});
+
+	while let Some((shard_id, event)) = events.next().await {
+		// something?
+		println!("got some event");
+	}
 
 	Ok(())
 }
