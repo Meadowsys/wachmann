65d7148cc119095c7de01de5b4421271e3fb1128
Autumn Meadow <blazeykirin@gmail.com>
2022-01-08 14:40:49 -0800

    escape message content

--- src/bot/logging/events/message_create.rs
+++ src/bot/logging/events/message_create.rs
@@ -8,8 +8,19 @@ pub async fn handle(msg: Box<MessageCreate>, db: &Arc<Database>) -> MainResult {
 	let id = msg.id.0;
 	let channel_id = msg.channel_id.0;
 	let author_id = msg.author.id.0;
-	let content = msg.content.clone();
 	let attachment_urls = msg.attachments.iter().map(|a| a.url.clone()).collect::<Vec<_>>();
+
+	let content = msg.content
+		.replace('*', "\\*")
+		.replace('#', "\\#")
+		.replace('/', "\\/")
+		.replace('(', "\\(")
+		.replace(')', "\\)")
+		.replace('[', "\\[")
+		.replace(']', "\\]")
+		.replace('_', "\\_");
+
 	db.save_message(&ClientMessage::SaveMessage { id, channel_id, author_id, content, attachment_urls }).await?;
+
 	Ok(())
 }
