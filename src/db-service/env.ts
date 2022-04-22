131e5597a312f6ed3cd584c3fb018916f0d8de63
Autumn Meadow <blazeykirin@gmail.com>
2022-04-22 16:24:37 -0700

    restore some db service stuff

--- src/db-service/env.ts
+++ src/db-service/env.ts
@@ -1,9 +1,8 @@
-function throw_no_var_found(...env_var: Array<string>): never {
-	throw new Error(`${
-		env_var.map(v => `process.env["${v}"]`).join(" or ")
-	} not found, please set ${
-		env_var.length === 1 ? "it" : "one of them"
-	}!`);
+function fatal_error_no_var_found(...env_var: Array<string>): never {
+	const map_var = (v: string) => `process.env["${v}"]`;
+	const or = " or ";
+	const end_phrase = env_var.length === 1 ? "it" : "one of them";
+	throw new Error(`${env_var.map(map_var).join(or)} not found, please set ${end_phrase}!`);
 }
 
 export type Env = ReturnType<typeof _get_env>;
@@ -11,34 +10,30 @@ export type Env = ReturnType<typeof _get_env>;
 function _get_env() {
 	let node_env = process.env.NODE_ENV;
 	if (!node_env || !(node_env === "development" || node_env === "production")) {
-		throw "check rollup config, there must be a typo in the define config for node_env";
+		throw new Error(`check rollup config, there must be a typo in the define config for process.env.NODE_ENV`);
 	}
 
-	let bot_token = process.env.TOKEN ?? process.env.BOT_TOKEN;
-	if (!bot_token) throw_no_var_found("TOKEN", "BOT_TOKEN");
-
 	let arango_url = process.env.ARANGO_URL ?? process.env.DB_URL;
-	if (!arango_url) throw_no_var_found("ARANGO_URL", "DB_URL");
+	if (!arango_url) fatal_error_no_var_found("ARANGO_URL", "DB_URL");
 
 	let arango_user = process.env.ARANGO_USER ?? process.env.DB_USER;
-	if (!arango_user) throw_no_var_found("ARANGO_USER", "DB_USER");
+	if (!arango_user) fatal_error_no_var_found("ARANGO_USER", "DB_USER");
 
 	let arango_password = process.env.ARANGO_PASSWORD ?? process.env.DB_PASSWORD;
-	if (!arango_password) throw_no_var_found("ARANGO_PASSWORD", "DB_PASSWORD");
+	if (!arango_password) fatal_error_no_var_found("ARANGO_PASSWORD", "DB_PASSWORD");
 
 	let arango_database = process.env.ARANGO_DATABASE ?? process.env.DB_DATABASE;
-	if (!arango_database) throw_no_var_found("ARANGO_DATABASE", "DB_DATABASE");
+	if (!arango_database) fatal_error_no_var_found("ARANGO_DATABASE", "DB_DATABASE");
 
 	let rv = {
 		node_env,
-		bot_token,
 		arango_url,
 		arango_user,
 		arango_password,
 		arango_database
 	};
 
-	// this will error if any of the above are undefined
+	// this will error if any of the above are undefined (must be defined)
 	// will be stripped out by rollup
 	let _typecheck: Record<string, string> = rv;
 
--- src/db-service/index.ts
+++ src/db-service/index.ts
@@ -1,1 +1,44 @@
-console.log("h");
+import "source-map-support/register";
+import "dotenv/config";
+
+import fs from "fs";
+import net from "net";
+import { arangojs } from "arangojs";
+import { get_env } from "./env";
+
+// everything prints to console.error, console.log only used to print server address
+let orig_console_log = console.log;
+console.log = console.error;
+
+let env = get_env();
+
+let db = arangojs({
+	url: env.arango_url,
+	databaseName: env.arango_database,
+	auth: {
+		username: env.arango_user,
+		password: env.arango_password
+	}
+});
+
+let sock_path = "db-service.sock";
+let i = 0;
+while (fs.existsSync(sock_path)) sock_path = `db-service-${++i}.sock`;
+
+const server = net.createServer();
+server.on("connection", handle_connection);
+
+await new Promise<void>(r => server.listen(sock_path, r))
+	.then(() => {
+		orig_console_log(sock_path)
+		orig_console_log = console.error; // its gone forever muehehhe
+	});
+
+server.once("close", () => console.log("server closed"));
+["SIGINT", "SIGTERM", "exit"].forEach(s => process.on(s, () => {
+	server.close(() => {});
+}));
+
+function handle_connection() {
+	// todo lol
+}
